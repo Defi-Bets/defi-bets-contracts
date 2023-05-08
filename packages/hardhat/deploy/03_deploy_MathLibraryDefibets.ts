07ba5587f28f52907d17aed8d917a4093080a1c5
larrry <larrry09@gmx.de>
2023-05-08 23:08:55 +0200

    add math library

---
+++ packages/hardhat/deploy/03_deploy_MathLibraryDefibets.ts
@@ -0,0 +1,19 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const deployMathLibraryDefibets: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deployer } = await hre.getNamedAccounts();
+
+  const { deploy } = hre.deployments;
+
+  await deploy("MathLibraryDefibets", {
+    from: deployer,
+    args: [],
+    log: true,
+    autoMine: true,
+  });
+};
+
+deployMathLibraryDefibets.tags = ["tags"];
+
+export default deployMathLibraryDefibets;
