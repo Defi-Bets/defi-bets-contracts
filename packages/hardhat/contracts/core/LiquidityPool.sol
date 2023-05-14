7750dd36d9f0918348e836ce413ba50a807c2e75
larrry <larrry09@gmx.de>
2023-05-14 12:30:45 +0200

    add Interface "inherit" also for Vault and Pool

--- packages/hardhat/contracts/core/LiquidityPool.sol
+++ packages/hardhat/contracts/core/LiquidityPool.sol
@@ -4,11 +4,12 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import '../interface/core/ILiquidityPool.sol';
 
 error LiquidityPool__AccessForbidden();
 error LiquidityPool__NotAllowedAmount();
 
-contract LiquidityPool is ERC20 {
+contract LiquidityPool is ERC20, ILiquidityPool {
 
     using SafeMath for uint256;
 
