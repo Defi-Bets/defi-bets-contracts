c536df02f69b486f4ba15cf9088c9dfa4bf6d983
Florian Meiswinkel <florian.meiswinkel@outlook.de>
2023-05-17 10:17:59 +0200

    integration of dmc testnet

---
+++ packages/hardhat/scripts/readDeFiChainTestnet.ts
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const blockNumber = await ethers.provider.getBlockNumber();
+
+    console.log(`DMC Testnet latest block number ${blockNumber}`);
+
+    const gasPrice = await ethers.provider.getGasPrice();
+    console.log(`🛢️ gas price: ${ethers.utils.formatUnits(gasPrice, "gwei")}`);
+
+    const network = await ethers.provider.getNetwork();
+
+    console.log(`⛓️ chain ID: ${network.chainId}`);
+}
+
+main().catch(error => {
+    console.error(error);
+    process.exitCode = 1;
+});
