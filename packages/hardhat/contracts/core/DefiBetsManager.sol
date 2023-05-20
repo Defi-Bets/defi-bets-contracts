0b9b6a9dfdfa5c33356fdc107a5a15d22b73a093
larrry <larrry09@gmx.de>
2023-05-20 12:03:41 +0200

    check that amount for fee calc is not null

--- packages/hardhat/contracts/core/DefiBetsManager.sol
+++ packages/hardhat/contracts/core/DefiBetsManager.sol
@@ -16,6 +16,7 @@ error DefiBetsManager__NoValidUnderlying();
 error DefiBetsManager__NoLiquidity();
 error DefiBetsManager__FeeNotAllowed();
 error DefiBetsManager__FeeWouldBeTooSmall();
+error DefiBetsManager__NullForbidden();
 
 
 /**
@@ -238,6 +239,9 @@ contract DefiBetsManager is Pausable,Ownable {
     }
 
     function calculateFee(uint256 _amount) public view returns(uint256){
+
+        _isNotNull(_amount);
+
         // multiply amount with fee. But because fee is in parts per 1 million (ppm), divide by 1 million
         uint256 _feeAmount = _amount.mul(feePpm).div(MILLION);  
 
@@ -248,6 +252,12 @@ contract DefiBetsManager is Pausable,Ownable {
         }
 
         return _feeAmount;
+    }
+
+    function _isNotNull(uint256 param) internal view {
+        if(0 == param) {
+            revert DefiBetsManager__NullForbidden();
+        }
     }
 
 }
