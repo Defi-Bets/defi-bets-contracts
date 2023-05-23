991c331601396043ab4e78c67173976707eb2528
Florian Meiswinkel <florian.meiswinkel@outlook.de>
2023-05-23 11:13:24 +0200

    Fix  issues from lint

--- packages/hardhat/deploy/04_deploy_MathLibraryDefibets.ts
+++ packages/hardhat/deploy/04_deploy_MathLibraryDefibets.ts
@@ -2,16 +2,16 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const deployMathLibraryDefibets: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const { deployer } = await hre.getNamedAccounts();
+    const { deployer } = await hre.getNamedAccounts();
 
-  const { deploy } = hre.deployments;
+    const { deploy } = hre.deployments;
 
-  await deploy("MathLibraryDefibets", {
-    from: deployer,
-    args: [],
-    log: true,
-    autoMine: true,
-  });
+    await deploy("MathLibraryDefibets", {
+        from: deployer,
+        args: [],
+        log: true,
+        autoMine: true,
+    });
 };
 
 deployMathLibraryDefibets.tags = ["tags"];
