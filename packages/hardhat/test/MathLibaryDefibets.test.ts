39467c46d8be139b18916e9411141e4723e1231f
Florian Meiswinkel <florian.meiswinkel@outlook.de>
2023-06-13 15:02:00 +0200

    fixing lint errors

--- packages/hardhat/test/MathLibaryDefibets.test.ts
+++ packages/hardhat/test/MathLibaryDefibets.test.ts
@@ -1,12 +1,9 @@
-import { expect } from "chai";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { ethers } from "hardhat";
 import { MathLibraryDefibets__factory, UseMathLibraryDefibets__factory } from "../typechain-types";
 
 describe("MathlibaryDefibets unit test", () => {
     async function deployMathlibaryFixture() {
-        const [user] = await ethers.getSigners();
-
         const MathLibraryDefiBets = (await ethers.getContractFactory(
             "MathLibraryDefibets",
         )) as MathLibraryDefibets__factory;
