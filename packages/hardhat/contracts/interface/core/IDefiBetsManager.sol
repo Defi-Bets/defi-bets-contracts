fbf2cbb5c6acd4e552cbe1fb3886a3ebed776ffa
larrry <larrry09@gmx.de>
2023-06-23 00:36:26 +0200

    add dynamic payout ratio calculation

--- packages/hardhat/contracts/interface/core/IDefiBetsManager.sol
+++ packages/hardhat/contracts/interface/core/IDefiBetsManager.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.9;
 
 interface IDefiBetsManager {
     function getLPTokenSupplies() external view returns(uint256,uint256);
+    function setNewPayoutFactor(uint256 _payoutFactor) external;
 }
